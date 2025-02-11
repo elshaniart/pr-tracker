18c85cd1d0b172b1c0d1e3f01c7d76ff07e40537
Art Elshani <elshaniart01@gmail.com>
2025-02-11 10:11:56 +0100

    restyle login and register

--- app/log-in/page.tsx
+++ app/log-in/page.tsx
@@ -5,6 +5,10 @@ import { useRouter } from "next/navigation";
 import supabase from "../helper/supabaseClient";
 import { signInWithGoogle } from "../helper/authHelpers";
 
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
+
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -38,48 +42,52 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Log In</h1>
-      <form onSubmit={handleLogin} className="w-64">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
-        {error && <p className="text-red-500 mb-4">{error}</p>}
+    <div
+      className={`flex flex-col items-center justify-center min-h-screen bg-[#15162e] ${montserrat.className}`}
+    >
+      <div className="text-center bg-gradient-to-r from-[#060B26] to-[#06275D] border-[2px] border-[#060B26] rounded-2xl p-8 text-white">
+        <h1 className="text-2xl font-bold mb-4">Log In</h1>
+        <form onSubmit={handleLogin} className="w-64">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
+          {error && <p className="text-red-500 mb-4">{error}</p>}
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 mb-4"
+          >
+            Sign In
+          </button>
+        </form>
         <button
-          type="submit"
-          className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600 mb-4"
+          onClick={handleGoogleSignIn}
+          className="w-64 bg-red-500 text-white p-2 rounded hover:bg-red-600 mb-4"
         >
-          Sign In
+          Sign In with Google
         </button>
-      </form>
-      <button
-        onClick={handleGoogleSignIn}
-        className="w-64 bg-red-500 text-white p-2 rounded hover:bg-red-600 mb-4"
-      >
-        Sign In with Google
-      </button>
-      <span className="text-sm text-gray-600">
-        Don't have an account?{" "}
-        <span
-          onClick={() => router.push("/register")}
-          className="text-blue-500 hover:underline cursor-pointer"
-        >
-          Register.
-        </span>
-      </span>
+        <div className="text-sm text-gray-600">
+          Don&apos;t have an account?{" "}
+          <span
+            onClick={() => router.push("/register")}
+            className="text-blue-500 hover:underline cursor-pointer"
+          >
+            Register.
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
--- app/register/page.tsx
+++ app/register/page.tsx
@@ -5,6 +5,10 @@ import { useRouter } from "next/navigation";
 import supabase from "../helper/supabaseClient";
 import { signInWithGoogle } from "../helper/authHelpers";
 
+import { Montserrat } from "next/font/google";
+
+const montserrat = Montserrat({ subsets: ["latin"], weight: ["400", "700"] });
+
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -60,48 +64,52 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-2xl font-bold mb-4">Register</h1>
-      <form onSubmit={handleRegister} className="w-64">
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="w-full p-2 mb-4 border rounded"
-          required
-        />
-        {error && <p className="text-red-500 mb-4">{error}</p>}
+    <div
+      className={`flex flex-col items-center justify-center min-h-screen bg-[#15162e] ${montserrat.className}`}
+    >
+      <div className="text-center bg-gradient-to-r from-[#060B26] to-[#06275D] border-[2px] border-[#060B26] rounded-2xl p-8 text-white">
+        <h1 className="text-2xl font-bold mb-4">Register</h1>
+        <form onSubmit={handleRegister} className="w-64">
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full p-2 mb-4 border rounded"
+            required
+          />
+          {error && <p className="text-red-500 mb-4">{error}</p>}
+          <button
+            type="submit"
+            className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 mb-4"
+          >
+            Register
+          </button>
+        </form>
         <button
-          type="submit"
-          className="w-full bg-green-500 text-white p-2 rounded hover:bg-green-600 mb-4"
+          onClick={handleGoogleSignIn}
+          className="w-64 bg-red-500 text-white p-2 rounded hover:bg-red-600 mb-4"
         >
-          Register
+          Sign In with Google
         </button>
-      </form>
-      <button
-        onClick={handleGoogleSignIn}
-        className="w-64 bg-red-500 text-white p-2 rounded hover:bg-red-600 mb-4"
-      >
-        Sign In with Google
-      </button>
-      <span className="text-sm text-gray-600">
-        Already have an account?{" "}
-        <span
-          onClick={() => router.push("/log-in")}
-          className="text-blue-500 hover:underline cursor-pointer"
-        >
-          Sign In.
-        </span>
-      </span>
+        <div className="text-sm text-gray-600">
+          Already have an account?{" "}
+          <span
+            onClick={() => router.push("/log-in")}
+            className="text-blue-500 hover:underline cursor-pointer"
+          >
+            Sign In.
+          </span>
+        </div>
+      </div>
     </div>
   );
 }
