2cbdc155a5180de28704e1a59f58d2990f2f459f
Art Elshani <elshaniart01@gmail.com>
2025-02-17 15:18:15 +0100

    dev mode for google sign in

--- app/helper/authHelpers.ts
+++ app/helper/authHelpers.ts
@@ -1,12 +1,18 @@
 import supabase from "./supabaseClient";
 
+const isDev = process.env.NODE_ENV === "development";
+const redirectTo = isDev
+  ? "http://localhost:3000/"
+  : "https://pr-tracker-taupe.vercel.app/";
+
 export const signInWithGoogle = async () => {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
+    options: { redirectTo },
   });
 
   if (error) {
-    throw new Error(error.message);
+    throw new Error("awd" + error.message);
   }
 
   return data;
--- app/helper/supabaseClient.ts
+++ app/helper/supabaseClient.ts
@@ -3,6 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL + "";
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY + "";
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true, // Ensures session is saved in localStorage
+    autoRefreshToken: true, // Automatically refresh token
+    detectSessionInUrl: true, // Needed for OAuth callbacks
+  },
+});
 
 export default supabase;
