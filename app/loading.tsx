0174a82a246f275891ea753c522483b0be49a795
Art Elshani <elshaniart01@gmail.com>
2025-02-27 23:58:37 +0100

    Update loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="w-screen h-screen flex items-center justify-center">
-      <Image src="/Loading.gif" alt="" width={128} height={128} />
+      <Image src="/Loading.gif" alt="" width={128} height={128} priority />
     </div>
   );
 };
